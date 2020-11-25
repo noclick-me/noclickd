d4a2d2f88ac5c5f9f59170d0287341a3c5316516
Leandro Lucarella <luca@llucax.com>
2020-11-25 11:22:20 +0100

    Move hello world to the / route

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{web, App, HttpServer, Responder};
 
 mod cli;
 
-async fn index() -> impl Responder {
+async fn root() -> impl Responder {
     "Hello world!"
 }
 
@@ -11,12 +11,8 @@ async fn main() -> std::io::Result<()> {
     let matches = cli::create_parser().get_matches();
 
     HttpServer::new(|| {
-        App::new().service(
-            // prefixes all resources and routes attached to it...
-            web::scope("/app")
-                // ...so this handles requests for `GET /app/index.html`
-                .route("/index.html", web::get().to(index)),
-        )
+        App::new()
+            .route("/", web::get().to(root))
     })
     .bind(matches.value_of("bind").unwrap())?
     .run()
