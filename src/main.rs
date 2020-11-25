94f677b874bf032e9ee5dc7153506e16117cdf16
Leandro Lucarella <luca@llucax.com>
2020-11-25 10:39:31 +0100

    Add command-line parsing to set the bind address

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,19 @@ async fn index() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let matches = clap::App::new("noclickd")
+          .version(env!("CARGO_PKG_VERSION"))
+          .author(env!("CARGO_PKG_AUTHORS"))
+          .about("noclick.me API server")
+          .arg(clap::Arg::with_name("bind")
+               .short("b")
+               .long("bind")
+               .default_value("127.0.0.1:8080")
+               .value_name("IP:PORT")
+               .help("Set the address/port to bind the server to")
+               .takes_value(true))
+          .get_matches();
+
     HttpServer::new(|| {
         App::new().service(
             // prefixes all resources and routes attached to it...
@@ -14,7 +27,7 @@ async fn main() -> std::io::Result<()> {
                 .route("/index.html", web::get().to(index)),
         )
     })
-    .bind("127.0.0.1:8080")?
+    .bind(matches.value_of("bind").unwrap())?
     .run()
     .await
 }
