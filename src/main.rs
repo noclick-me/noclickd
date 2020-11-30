b8359ea2deb6888372f1c01d613882b85e076cc5
Leandro Lucarella <luca@llucax.com>
2020-11-30 12:50:51 +0100

    Fix code formatting

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,8 @@ async fn root() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     let matches = cli::create_parser().get_matches();
 
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(root))
-    })
-    .bind(matches.value_of("bind").unwrap())?
-    .run()
-    .await
+    HttpServer::new(|| App::new().route("/", web::get().to(root)))
+        .bind(matches.value_of("bind").unwrap())?
+        .run()
+        .await
 }
