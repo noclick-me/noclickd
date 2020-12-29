ba40eea648c5f934fb53e567b0a35f7a901921ea
Leandro Lucarella <luca@llucax.com>
2020-12-29 16:07:39 +0100

    Add TLS support using rustls

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,20 @@ use crate::config::Config;
 use crate::state::SharedState;
 
 use actix_web::{web, App, HttpServer};
+use rustls::internal::pemfile::{certs, pkcs8_private_keys};
+use rustls::{NoClientAuth, ServerConfig};
+use std::fs::File;
+use std::io::BufReader;
+
+fn tls_config() -> ServerConfig {
+    let mut config = ServerConfig::new(NoClientAuth::new());
+    let cert_file = &mut BufReader::new(File::open("cert.pem").unwrap());
+    let key_file = &mut BufReader::new(File::open("key.pem").unwrap());
+    let cert_chain = certs(cert_file).unwrap();
+    let mut keys = pkcs8_private_keys(key_file).unwrap();
+    config.set_single_cert(cert_chain, keys.remove(0)).unwrap();
+    config
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -23,7 +37,7 @@ async fn main() -> std::io::Result<()> {
             .service(service::url::mount(web::scope("/url")))
             .service(service::view::mount(web::scope("/")))
     })
-    .bind(matches.value_of("bind").unwrap())?
+    .bind_rustls(matches.value_of("bind").unwrap(), tls_config())?
     .run()
     .await
 }
