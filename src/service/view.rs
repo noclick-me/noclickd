ed488e207bd9f38256af5eff552f13a9aeee7699
Leandro Lucarella <luca@llucax.com>
2020-12-29 22:06:40 +0100

    Fix handling of view URLs

--- src/service/view.rs
+++ src/service/view.rs
@@ -6,12 +6,13 @@ pub fn mount(scope: Scope) -> Scope {
     scope.service(view)
 }
 
-#[get("{id}")]
+#[get("{id}/{rest:.*}")]
 async fn view(
-    web::Path((id,)): web::Path<(String,)>,
+    web::Path((id, rest)): web::Path<(String, String)>,
     state: web::Data<SharedState>,
 ) -> impl Responder {
     dbg!(&id);
+    dbg!(&rest);
     let read_db = state.db.read().unwrap();
     let entry = read_db.get(&id).unwrap();
 
