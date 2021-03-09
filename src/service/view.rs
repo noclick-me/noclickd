5cf5735d16b4dcc816117a54e9bb70427ad137ec
Leandro Lucarella <luca@llucax.com>
2021-03-09 12:22:33 +0100

    view: Fix Location URL
    The Location header URL is now being set to the source_url (it was being
    set to the noclick_url mistakenly).
    
    Signed-off-by: Leandro Lucarella <luca@llucax.com>

--- src/service/view.rs
+++ src/service/view.rs
@@ -14,17 +14,16 @@ async fn view(
     dbg!(&rest);
 
     let mut db_conn = db_pool.acquire().await.unwrap();
-    let row = sqlx::query(r"SELECT noclick_url FROM urls WHERE id = $1")
+    let row = sqlx::query(r"SELECT source_url FROM urls WHERE id = $1")
         .bind(&id)
         .fetch_one(&mut db_conn)
         .await
         .unwrap();
 
-    use crate::config::config;
     use actix_web::{http::header::LOCATION, HttpResponse};
     use sqlx::Row;
-    let noclick_url: &str = row.try_get("noclick_url").unwrap();
-    let mut url = format!("{}/{}/{}", config().link.base_url, id, noclick_url);
-    url.truncate(config().link.max_length);
-    HttpResponse::Found().set_header(LOCATION, url).finish()
+    let source_url: &str = row.try_get("source_url").unwrap();
+    HttpResponse::Found()
+        .set_header(LOCATION, source_url)
+        .finish()
 }
