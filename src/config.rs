f716b47c651f77db1d1e6df4b60c20b086c184e2
Malhar Vora <mlvora.2010@gmail.com>
2022-02-23 16:34:23 +0530

    Make server configurable using config-rs
    Signed-off-by: Malhar Vora <mlvora.2010@gmail.com>

--- src/config.rs
+++ src/config.rs
@@ -1,54 +1,56 @@
+use config::ConfigError;
+use config::{File, FileFormat};
 use lazy_static::lazy_static;
+use serde::Deserialize;
+
 lazy_static! {
-    static ref CONF: Config = Config::default();
+    static ref CONF: Config = Config::new("config.toml".to_string()).unwrap();
 }
 
 pub fn config() -> &'static Config {
     &CONF
 }
-
-#[derive(Debug)]
+#[derive(Debug, Deserialize)]
 pub struct ApiConfig {
     pub host: String,
 }
 
-use actix_web::http::{HeaderName, Method};
-#[derive(Debug)]
+#[derive(Debug, Deserialize)]
 pub struct CorsConfig {
     pub allowed_origins: Vec<String>,
-    pub allowed_methods: Vec<Method>,
-    pub allowed_headers: Vec<HeaderName>,
+    pub allowed_methods: Vec<String>,
+    pub allowed_headers: Vec<String>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize)]
 pub struct DbConfig {
     pub url: String,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize)]
 pub struct LimitsConfig {
     pub global_requests_per_day: usize,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize)]
 pub struct LinkConfig {
     pub base_url: String,
     pub max_length: usize,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize)]
 pub struct ViewerConfig {
     pub host: String,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize)]
 pub struct WebappConfig {
     pub redirect_from_path: String,
     pub redirect_from_host: String,
     pub redirect_to: String,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize)]
 pub struct Config {
     pub api: ApiConfig,
     pub cors: CorsConfig,
@@ -59,47 +61,17 @@ pub struct Config {
     pub webapp: WebappConfig,
 }
 
-impl Default for Config {
-    fn default() -> Self {
-        use actix_web::http::header;
-        Self {
-            api: ApiConfig {
-                host: "api.noclick.me".to_string(),
-            },
-            cors: CorsConfig {
-                allowed_origins: vec![
-                    "https://noclick.me",
-                    "https://web.noclick.me",
-                    "https://www.noclick.me",
-                    "https://test.noclick.me",
-                    "https://app.noclick.me",
-                    "https://test.app.noclick.me",
-                ]
-                .iter()
-                .map(|s| s.to_string())
-                .collect(),
-                allowed_methods: vec![Method::GET, Method::POST],
-                allowed_headers: vec![header::ACCEPT, header::CONTENT_TYPE],
-            },
-            db: DbConfig {
-                // mode=read-write-create if it doesn't exist
-                url: "sqlite://db.sqlite?mode=rwc".to_string(),
-            },
-            limits: LimitsConfig {
-                global_requests_per_day: 10_000,
-            },
-            link: LinkConfig {
-                base_url: "https://noclick.me".to_string(),
-                max_length: 4096,
-            },
-            viewer: ViewerConfig {
-                host: "noclick.me".to_string(),
-            },
-            webapp: WebappConfig {
-                redirect_from_path: "/".to_string(),
-                redirect_from_host: "noclick.me".to_string(),
-                redirect_to: "https://app.noclick.me/".to_string(),
-            },
-        }
+impl Config {
+    fn new(config_path: String) -> Result<Self, ConfigError> {
+        config::Config::builder()
+            .set_default("db.url", "sqlite://db.sqlite?mode=rwc".to_string())?
+            .set_default("cors.allowed_methods", vec!["GET", "POST"])?
+            .set_default("cors.allowed_headers", vec!["ACCEPT", "CONTENT_TYPE"])?
+            .set_default("limits.global_requests_per_day", 10000)?
+            .set_default("link.max_length", 4096)?
+            .set_default("webapp.redirect_from_path", "/".to_string())?
+            .add_source(File::new(&config_path, FileFormat::Toml))
+            .build()?
+            .try_deserialize()
     }
 }
